cafbc10fbf04588d3018dc7d14f5a4afffcb40d5
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2024-09-03 19:29:07 +0530

    Working with addTodo, updateTodo Functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import { useState } from 'react'
 import './App.css'
+import { TodoProvider } from './contexts'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [{id: Date.now(), ...todo}, ...prev])
+  }
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo,id === todo.id ? todo : prevTodo)))
+  }
+
+  
 
   return (
-    <>
+    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
       <h1 className='text-3xl font-bold underline'>Hello World</h1>
-    </>
+    </TodoProvider>
   )
 }
 
