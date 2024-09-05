5b782cb32040094a8222a49ef2c76d61e003773f
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2024-09-05 23:20:53 +0530

    deleteTodo

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,9 @@ function App() {
     setTodos((prev) => prev.map((prevTodo) => (prevTodo,id === todo.id ? todo : prevTodo)))
   }
 
-  
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !==id))
+  }
 
   return (
     <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
