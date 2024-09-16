293c005d875160a03b67f16ac2403ebd8bdda4d6
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2024-09-16 19:49:47 +0530

    Handling inconsistencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { TodoProvider } from "./contexts"
+import { TodoProvider } from "./contexts/index"
 import { useEffect } from 'react'
 import { TodoForm, TodoItem } from './components'
 
