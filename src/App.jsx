dd0a8b1ee8c43363ca4026fdd9663da9e4a31507
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2025-04-18 11:07:36 +0530

    prod error resolve2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import { TodoProvider } from './contexts/TodoContext'
+import { TodoProvider } from './context/TodoContext.js'
 import { useEffect } from 'react'
 import { TodoForm, TodoItem } from './components'
 
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,28 +1,28 @@
 import React from 'react'
 import { useState } from 'react'
-import { useTodo } from '../contexts/TodoContext'
+import { useTodo } from '../context/TodoContext'
 
 function TodoForm() {
-    const [todo, setTodo] = useState("")
-    const {addTodo} = useTodo()
+  const [todo, setTodo] = useState("")
+  const { addTodo } = useTodo()
 
-    const add = (e) => {
-        e.preventDefault()
-        if (!todo) return
-        addTodo({todo, completed: false})
-        setTodo("")
-    }
+  const add = (e) => {
+    e.preventDefault()
+    if (!todo) return
+    addTodo({ todo, completed: false })
+    setTodo("")
+  }
   return (
     <form onSubmit={add} className='flex'>
-        <input type="text" 
+      <input type="text"
         placeholder='Write Todo...'
         className='w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5'
         value={todo}
         onChange={(e) => setTodo(e.target.value)}
-        />
-        <button type="submit"
+      />
+      <button type="submit"
         className='rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0'
-        >Add</button>
+      >Add</button>
     </form>
   )
 }
